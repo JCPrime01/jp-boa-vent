89686754e2c5c3ff481514b4e9b3e0c0a02a4c35
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-28 14:26:28 -0300

    chore: update shareCode to APV60O6DH6Z

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=N3L1HO3OGOR&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=APV60O6DH6Z&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
