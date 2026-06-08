78a96233c5bdb03e8653523cab4dd2485cc74247
JCPrime01 <vanzokiller@gmail.com>
2026-06-08 11:38:35 -0300

    chore: shareCode VVNBHYB4VP1 -> W6INBS94O13 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=VVNBHYB4VP1&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=W6INBS94O13&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
