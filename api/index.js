9b6d96e0d06e22a603791322231e43910371037c
JCPrime01 <vanzokiller@gmail.com>
2026-06-24 12:10:33 -0300

    chore: shareCode 6GUOY5FMOQZ -> FKPT43ZGHFL (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7175&shareCode=6GUOY5FMOQZ&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7175&shareCode=FKPT43ZGHFL&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
